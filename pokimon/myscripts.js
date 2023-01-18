19566f15eceede95b88cd7162964eebcc23436f7
shaikhusama155 <shaikhusama155@gmail.com>
2023-01-18 06:07:57 +0500

    add some

--- pokimon/myscripts.js
+++ pokimon/myscripts.js
@@ -28,3 +28,12 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
